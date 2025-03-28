24aa4ca7c2be4afd026e82e63644d60246a6b772
gkourouma <gkourouma2@gmail.com>
2025-03-28 13:43:40 -0500

    Added links for each pokemon character

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,16 +1,20 @@
-const PokemonList = (props) => {
-    return(
-        <>
-        <h2>Pokemon</h2>
-        <ul>
-            {props.pokemon.map((currentPokemon) =>(
-                <li key={currentPokemon.name}>
-                    {currentPokemon.name}
-                </li>
-            ) )}
-        </ul>
-        </>
-    )
-}
+import { Link } from "react-router";
 
-export default PokemonList
+const PokemonList = (props) => {
+  return (
+    <>
+      <h2>Pokemon</h2>
+      <ul>
+        {props.pokemon.map((currentPokemon) => (
+          <li key={currentPokemon.name}>
+            <Link to={`/pokemon/${currentPokemon._id}`}>
+              {currentPokemon.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default PokemonList;
